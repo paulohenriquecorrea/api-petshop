514c4e7dd4b92a0e66a34cbdd45b5e55f74c88fd
pauloh <ph.sistemas.ti@gmail.com>
2021-03-31 01:01:02 -0400

    Final aula 3 - Educando sua API

--- api/rotas/fornecedores/TabelaFornecedor.js
+++ api/rotas/fornecedores/TabelaFornecedor.js
@@ -30,4 +30,12 @@ module.exports = {
       },
     });
   },
+
+  remover(id) {
+    return Modelo.destroy({
+      where: {
+        id: id,
+      },
+    });
+  },
 };
--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -4,7 +4,7 @@ const Fornecedor = require('./Fornecedor');
 
 roteador.get('/', async (req, res) => {
   const resultado = await TabelaFornecedor.listar();
-
+  res.status(200);
   res.send(JSON.stringify(resultado));
 });
 
@@ -13,8 +13,10 @@ roteador.get('/:idFornecedor', async (req, res) => {
     const id = req.params.idFornecedor;
     const fornecedor = new Fornecedor({ id: id });
     await fornecedor.carregar();
+    res.status(200);
     res.send(JSON.stringify(fornecedor));
   } catch (error) {
+    res.status(404);
     res.send(
       JSON.stringify({
         mensagem: error.message,
@@ -24,10 +26,20 @@ roteador.get('/:idFornecedor', async (req, res) => {
 });
 
 roteador.post('/', async (req, res) => {
-  const dadosRecebidos = req.body;
-  const fornecedor = new Fornecedor(dadosRecebidos);
-  await fornecedor.criar();
-  res.send(JSON.stringify(fornecedor));
+  try {
+    const dadosRecebidos = req.body;
+    const fornecedor = new Fornecedor(dadosRecebidos);
+    await fornecedor.criar();
+    res.status(201);
+    res.send(JSON.stringify(fornecedor));
+  } catch (error) {
+    res.status(400);
+    res.send(
+      JSON.stringify({
+        mensagem: error.message,
+      })
+    );
+  }
 });
 
 roteador.put('/:idFornecedor', async (req, res) => {
@@ -37,11 +49,31 @@ roteador.put('/:idFornecedor', async (req, res) => {
     const dados = Object.assign({}, dadosRecebidos, { id: id });
     const fornecedor = new Fornecedor(dados);
     await fornecedor.atualizar();
+    res.status(204);
     res.end();
   } catch (erro) {
+    res.status(400);
     res.send(
       JSON.stringify({
         mensagem: erro.message,
+      })
+    );
+  }
+});
+
+roteador.delete('/:idFornecedor', async (req, res) => {
+  try {
+    const id = req.params.idFornecedor;
+    const fornecedor = new Fornecedor({ id: id });
+    await fornecedor.carregar();
+    await fornecedor.remover();
+    res.status(204);
+    res.end();
+  } catch (error) {
+    res.status(404);
+    res.send(
+      JSON.stringify({
+        mensagem: error.message,
       })
     );
   }
